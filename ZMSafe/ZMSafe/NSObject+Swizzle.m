13c73c2518766775ed0e132978291e093fe41b3c
xiezhimin <741937337@qq.com>
2018-07-03 19:38:03 +0800

    添加交换保护措施

--- ZMSafe/ZMSafe/NSObject+Swizzle.m
+++ ZMSafe/ZMSafe/NSObject+Swizzle.m
@@ -26,7 +26,17 @@
     
     Method srcMethod = class_getInstanceMethod(srcClass, srcSel);
     Method swizzledMethod = class_getInstanceMethod([self class], swizzledSel);
-    method_exchangeImplementations(srcMethod, swizzledMethod);
+    
+    //加一层保护措施，如果添加成功，则表示该方法是存在于父类中，不能交换父类的方法,否则父类的对象调用该方法会crash；添加失败则表示本类存在该方法
+    BOOL addMethod = class_addMethod(srcClass, srcSel, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+    if (addMethod)
+    {
+        //再将原有的实现替换到swizzledMethod方法上，从而实现方法的交换，并且未影响到父类方法的实现
+        class_replaceMethod(srcClass, swizzledSel, method_getImplementation(srcMethod), method_getTypeEncoding(srcMethod));
+    }else
+    {
+        method_exchangeImplementations(srcMethod, swizzledMethod);
+    }
 }
 
 
@@ -45,6 +55,15 @@
     
     Method srcMethod = class_getClassMethod(srcClass, srcSel);
     Method swizzledMethod = class_getClassMethod([self class], swizzledSel);
-    method_exchangeImplementations(srcMethod, swizzledMethod);
+    
+    BOOL addMethod = class_addMethod(srcClass, srcSel, method_getImplementation(srcMethod), method_getTypeEncoding(srcMethod));
+    if (addMethod)
+    {
+        class_replaceMethod(srcClass, swizzledSel, method_getImplementation(srcMethod), method_getTypeEncoding(srcMethod));
+    }else
+    {
+        method_exchangeImplementations(srcMethod, swizzledMethod);
+    }
 }
+
 @end
