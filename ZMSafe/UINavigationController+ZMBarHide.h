963d2fb25fac1b3f8fdd14f96ce1fb150dcba03d
xiezhimin <xiezm@dachentech.com.cn>
2020-03-12 14:37:41 +0800

    添加默认忽略问价

--- ZMSafe/UINavigationController+ZMBarHide.h
+++ ZMSafe/UINavigationController+ZMBarHide.h
@@ -1,32 +1,46 @@
 //
-//  UINavigationController+ZMBarHide.h
-//  ZMSafe
+//  UINavigationController+ZMBarHidden.h
+//  PatientCircle
 //
-//  Created by xzm on 2018/7/10.
+//  Created by x on 2018/7/10.
 //  Copyright © 2018年 CHT-Technology. All rights reserved.
 //  解决导航栏隐藏的控制器手势返回动画过渡问题，关闭导航穿透 + 关闭动画 = 滑动返回顶部黑条，动画关闭过渡不自然
 //  解决修改导航栏导致滑动返回手势失效问题、跟控制器滑动页面假死
+//
 
 #import <UIKit/UIKit.h>
 
 @interface UINavigationController (ZMBarHide)<UIGestureRecognizerDelegate>
 
-/** 忽略navigationbar自动显示的控制器集合，默认为空，相当于一个黑名单
-    当控制器在viewDidLoad中隐藏了navigationbar，则把该控制器添加至该集合
- */
-@property (nonatomic,strong) NSMutableSet<__kindof NSString *> *zm_ignoreVCs;
+extern void ZMBarHidden_ExchangeImplementation(Class c,SEL srcSel,SEL swizzledSel);
 
 /** 是否自动控制navigationbar显示隐藏，默认为YES */
 @property (nonatomic,assign) BOOL zm_barAppearaceEnabled;
 
 @end
 
+
+
 @interface UIViewController (ZMBarHide)
 
 /** 导航栏是否隐藏，默认为NO */
 @property (nonatomic,assign) BOOL zm_navigationBarHidden;
-
+/** 导航栏下面的线是否隐藏，默认为NO */
+@property (nonatomic,assign) BOOL zm_navigationBarLineHidden;
 /** 滑动返回手势失效，默认为NO */
 @property (nonatomic,assign) BOOL zm_interactivePopDisabled;
 
+
 @end
+
+
+
+@interface UIApplication (ZMBarHide)
+
+/** 忽略navigationbar自动显示的控制器集合，默认为空，相当于一个黑名单
+ 当控制器在viewDidLoad中隐藏了navigationbar，则把该控制器添加至该集合
+ */
+@property (nonatomic,strong) NSMutableSet<__kindof NSString *> *zm_ignoreNavHideVCs;
+
+@end
+
--- ZMSafe/UINavigationController+ZMBarHide.m
+++ ZMSafe/UINavigationController+ZMBarHide.m
@@ -1,13 +1,13 @@
 //
-//  UINavigationController+ZMBarHide.m
-//  ZMSafe
+//  UINavigationController+ZMBarHidden.m
+//  PatientCircle
 //
-//  Created by xzm on 2018/7/10.
-//  Copyright © 2018年 CHT-Technology. All rights reserved.
+//  Created by xzm on 2018/5/29.
+//  Copyright © 2018年 Dachen. All rights reserved.
 //
 
 #import "UINavigationController+ZMBarHide.h"
-#import "NSObject+Swizzle.h"
+#import <objc/runtime.h>
 
 @interface ZMPopGestureRecognizerDelegate : NSObject <UIGestureRecognizerDelegate>
 
@@ -33,24 +33,74 @@
 
 @end
 
+@implementation UIApplication (ZMBarHide)
+
+#pragma mark -- getter && setter
+- (NSMutableSet <__kindof NSString *> *)zm_ignoreNavHideVCs
+{
+    NSMutableSet *mSet = objc_getAssociatedObject(self, _cmd);
+    if (!mSet)
+    {
+        NSArray *defaultArray = @[@"CAMViewfinderViewController",@"LFCameraDisplayController",@"LFCameraTakeViewController",@"LFCameraPickerController",@"UIImagePickerController",@"CAMImagePickerCameraViewController",@"CAMPreviewViewController",@"MFMessageComposeViewController",@"CKSMSComposeController",@"CKSMSComposeRemoteViewController"];
+        mSet = [NSMutableSet setWithArray:defaultArray];
+        self.zm_ignoreNavHideVCs = mSet;
+    }
+    return mSet;
+}
+
+- (void)setZm_ignoreNavHideVCs:(NSMutableSet<__kindof NSString *> *)zm_ignoreNavHideVCs
+{
+    objc_setAssociatedObject(self, @selector(zm_ignoreNavHideVCs), zm_ignoreNavHideVCs, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+
+@end
+
 
 @implementation UIViewController (ZMBarHide)
+
+void ZMBarHidden_ExchangeImplementation(Class srcClass,SEL srcSel,SEL swizzledSel)
+{
+    Method srcMethod = class_getInstanceMethod(srcClass, srcSel);
+    Method swizzledMethod = class_getInstanceMethod(srcClass, swizzledSel);
+    //该类为nil || 该类或者其父类没有这个方法，则不执行交换
+    if (!srcClass || !srcMethod || !swizzledMethod) return;
+    
+    //加一层保护措施，如果添加成功，则表示该方法不存在于本类，而是存在于父类中，不能交换父类的方法,否则父类的对象调用该方法会crash；添加失败则表示本类存在该方法
+    BOOL addMethod = class_addMethod(srcClass, srcSel, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+    if (addMethod)
+    {
+        //再将原有的实现替换到swizzledMethod方法上，从而实现方法的交换，并且未影响到父类方法的实现
+        class_replaceMethod(srcClass, swizzledSel, method_getImplementation(srcMethod), method_getTypeEncoding(srcMethod));
+    }else
+    {
+        method_exchangeImplementations(srcMethod, swizzledMethod);
+    }
+}
 
 + (void)load
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        [self zm_swizzleInstanceMethodWithSrcClass:[self class] srcSel:@selector(viewWillAppear:) swizzledSel:@selector(zm_viewWillAppear:)];
+        ZMBarHidden_ExchangeImplementation([self class],@selector(viewWillAppear:),@selector(zm_viewWillAppear:));
     });
 }
 
 #pragma mark -- swizzling methods
 - (void)zm_viewWillAppear:(BOOL)animated
 {
-    if(![self.navigationController.zm_ignoreVCs containsObject:NSStringFromClass([self class])]
-       && self.navigationController.zm_barAppearaceEnabled)
+    //该控制器是否是子控制器，如果是，则不影响父控制器对导航的操作
+    UIViewController *parentVC = self.parentViewController;
+    BOOL isSubVC = (![parentVC isKindOfClass:[UINavigationController class]] && ![parentVC isKindOfClass:[UITabBarController class]]);
+    
+    if(![[UIApplication sharedApplication].zm_ignoreNavHideVCs containsObject:NSStringFromClass([self class])]
+       && self.navigationController.zm_barAppearaceEnabled && !isSubVC)
     {
         [self.navigationController setNavigationBarHidden:self.zm_navigationBarHidden animated:animated];
+        UIView *line = [self.navigationController.navigationBar viewWithTag:113];
+        if (line) {
+            line.hidden = self.zm_navigationBarLineHidden;
+        }
     }
     [self zm_viewWillAppear:animated];
 }
@@ -64,6 +114,16 @@
 - (void)setZm_navigationBarHidden:(BOOL)zm_navigationBarHidden
 {
     objc_setAssociatedObject(self, @selector(zm_navigationBarHidden), @(zm_navigationBarHidden), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (BOOL)zm_navigationBarLineHidden
+{
+    return [objc_getAssociatedObject(self, _cmd) boolValue];
+}
+
+- (void)setZm_navigationBarLineHidden:(BOOL)zm_navigationBarLineHidden
+{
+    objc_setAssociatedObject(self, @selector(zm_navigationBarLineHidden), @(zm_navigationBarLineHidden), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 - (BOOL)zm_interactivePopDisabled
@@ -85,7 +145,7 @@
 {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        [self zm_swizzleInstanceMethodWithSrcClass:[self class] srcSel:@selector(viewDidLoad) swizzledSel:@selector(zm_viewDidLoad)];
+        ZMBarHidden_ExchangeImplementation([self class],@selector(viewDidLoad),@selector(zm_viewDidLoad));
     });
 }
 
@@ -97,27 +157,12 @@
 }
 
 #pragma mark -- getter && setter
-- (NSMutableSet <__kindof NSString *> *)zm_ignoreVCs
-{
-    NSMutableSet *mSet = objc_getAssociatedObject(self, _cmd);
-    if (!mSet)
-    {
-        mSet = [NSMutableSet set];
-        self.zm_ignoreVCs = mSet;
-    }
-    return mSet;
-}
-
-- (void)setZm_ignoreVCs:(NSMutableSet<__kindof NSString *> *)zm_ignoreVCs
-{
-    objc_setAssociatedObject(self, @selector(zm_ignoreVCs), zm_ignoreVCs, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
 
 - (BOOL)zm_barAppearaceEnabled
 {
     NSNumber *number = objc_getAssociatedObject(self, _cmd);
     if(number){
-      return number.boolValue;
+        return number.boolValue;
     }
     self.zm_barAppearaceEnabled = YES;
     return YES;
@@ -141,4 +186,3 @@
 }
 
 @end
-
